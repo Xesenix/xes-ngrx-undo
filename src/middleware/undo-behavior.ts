f3d79539feb9668be814101ebb5da71bf16cebad
Paweł Kapalla <pkapalla@xesenix.pl>
2017-11-04 23:34:42 +0100

    fix(aot): fix ahead of time requiring function

--- src/middleware/undo-behavior.ts
+++ src/middleware/undo-behavior.ts
@@ -2,24 +2,24 @@ import { ActionReducer, Action } from '@ngrx/store';
 
 import { UNDO_ACTION } from '../actions/undo';
 
-export const undoBehavior = (bufferSize = 100) => (
-	rootReducer: ActionReducer<any>
-): ActionReducer<any> => {
-	let executedActions: Array<Action> = [];
-	let rootState: any = undefined;
-	return (state: any, action: any) => {
-		if (action.type === UNDO_ACTION) {
-			const index = executedActions.lastIndexOf(action.payload);
-			executedActions.splice(index, 1);
-			return executedActions.reduce(rootReducer, rootState);
-		}
-		executedActions.push(action);
-		let updatedState = rootReducer(state, action);
-		if (executedActions.length === bufferSize + 1) {
-			let firstAction = executedActions[0];
-			rootState = rootReducer(rootState, firstAction);
-			executedActions = executedActions.slice(1, bufferSize + 1);
-		}
-		return updatedState;
+export function undoBehavior(bufferSize = 100) {
+	return (rootReducer: ActionReducer<any>): ActionReducer<any> => {
+		let executedActions: Array<Action> = [];
+		let rootState: any = undefined;
+		return (state: any, action: any) => {
+			if (action.type === UNDO_ACTION) {
+				const index = executedActions.lastIndexOf(action.payload);
+				executedActions.splice(index, 1);
+				return executedActions.reduce(rootReducer, rootState);
+			}
+			executedActions.push(action);
+			let updatedState = rootReducer(state, action);
+			if (executedActions.length === bufferSize + 1) {
+				let firstAction = executedActions[0];
+				rootState = rootReducer(rootState, firstAction);
+				executedActions = executedActions.slice(1, bufferSize + 1);
+			}
+			return updatedState;
+		};
 	};
-};
+}
